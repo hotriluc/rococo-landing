4d8d145d5e47e9e9a12086bb1d7f79b3549011be
Luc Ho <hotriluc97@gmail.com>
2023-01-17 15:44:07 +0100

    animate characteristic section

--- main.js
+++ main.js
@@ -6,8 +6,6 @@ gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray(".panel");
 
-// console.log(sections);
-
 let scrollTween = gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
   ease: "none",
@@ -15,23 +13,26 @@ let scrollTween = gsap.to(sections, {
     trigger: ".container-horizontal",
     pin: true,
     scrub: 1,
-    // markers: true,
-    snap: 1 / (sections.length - 1),
+    // snap: {
+    //   snapTo: 1 / (sections.length - 1),
+    //   duration: 1.2,
+    //   ease: "Power1.easeOut",
+    // },
     end: () =>
       "+=" + document.querySelector(".container-horizontal").offsetWidth,
   },
 });
 
 gsap.from(".quote", {
-  x: 100,
+  y: 100,
   opacity: 0,
+  stagger: 0.25,
   scrollTrigger: {
     trigger: ".characteristic",
-    markers: true,
     horizontal: true,
     containerAnimation: scrollTween,
-    start: "-40% left",
-    end: "5% end",
+    start: "-30% left",
+    end: "left end",
     scrub: true,
   },
 });
